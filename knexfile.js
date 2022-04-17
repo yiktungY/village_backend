43f4abea928dc63017db6fe0cbc8b2e30d5d8fd7
yiktungyeung <yeungyiktung@CityDog-MacPro.ht.home>
2022-04-17 16:07:18 -0400

    try

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,20 @@
-module.exports = {
+// module.exports = {
+//   development: {
+//     client: "mysql",
+//     connection: {
+//       host: "127.0.0.1",
+//       user: "root",
+//       password: "rootroot",
+//       database: "village_database",
+//       charset: "utf8",
+//     },
+//   },
+//   production: {
+//     client: "mysql",
+//     connection: process.env.JAWSDB_URL,
+//   },
+// };
+const connections = {
   development: {
     client: "mysql",
     connection: {
@@ -14,3 +30,8 @@ module.exports = {
     connection: process.env.JAWSDB_URL,
   },
 };
+
+module.exports =
+  process.env.NODE_ENV === "production"
+    ? connections.production
+    : connections.development;
