f273a6a593f9e2973335f01f97b94a692f0b0a02
yiktungyeung <yeungyiktung@CityDog-MacPro.ht.home>
2022-04-24 00:27:13 -0400

    test

--- routes/auth.js
+++ routes/auth.js
@@ -47,40 +47,39 @@ router.get("/success-callback", (req, res) => {
   }
 });
 
-// router.push("/newlogin", (req, res) => {
-//   knex("users")
-//     .select("id")
-//     .where({ google_id: profile.id })
-//     .then((user) => {
-//       if (user.length) {
-//         console.log(user, "found the users and successfully login");
-//         // If user is found, pass the user object to serialize function
-//         done(null, user[0]);
-//       } else {
-//         // If user isn't found, we create a record
-//         console.log("cannot find id, insert to knex table");
-//         knex("users")
-//           .insert({
-//             google_id: profile.id,
-//             email: profile.email,
-//             avatar_url: profile.picture,
-//             displayName: profile.displayName,
-//             givenName: profile.given_name,
-//             familyName: profile.family_name,
-//           })
-//           .then((user) => {
-//             console.log(user, "user");
-//             // Pass the user object to serialize function
-//             done(null, user[0]);
-//           })
-//           .catch((err) => {
-//             console.log("Error creating a user", err);
-//           });
-//       }
-//     })
-//     .catch((err) => {
-//       console.log("Error fetching a user", err);
-//     });
-// });
+router.post("/newlogin", (req, res) => {
+  knex("users")
+    .select("id")
+    .where({ google_id: res.id })
+    .then((user) => {
+      if (user.length) {
+        console.log(user, "found the users and successfully login");
+        // If user is found, pass the user object to serialize function
+        done(null, user[0]);
+      } else {
+        // If user isn't found, we create a record
+        console.log("cannot find id, insert to knex table");
+        knex("users")
+          .insert({
+            google_id: res.uid,
+            email: res.email,
+            avatar_url:
+              "https://firebasestorage.googleapis.com/v0/b/village-345022.appspot.com/o/files%2F124465293-56a001375f9b58eba4ae696f.jpeg?alt=media&token=9070e549-4adb-4356-a141-41119dbdfaa9",
+            displayName: "test",
+          })
+          .then((user) => {
+            console.log(user, "user");
+            // Pass the user object to serialize function
+            done(null, user[0]);
+          })
+          .catch((err) => {
+            console.log("Error creating a user", err);
+          });
+      }
+    })
+    .catch((err) => {
+      console.log("Error fetching a user", err);
+    });
+});
 
 module.exports = router;
