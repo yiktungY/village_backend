49413b345e898694f362bae9f5a55b0159c38ab8
yiktungyeung <yeungyiktung@CityDog-MacPro.ht.home>
2022-04-23 23:29:30 -0400

    test

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
-const { constants } = require("buffer");
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+const knex = require("knex")(require("../knexfile.js").production);
 
 require("dotenv").config();
 
@@ -26,7 +26,6 @@ router.get(
 );
 
 router.get("/profile", (req, res) => {
-
   if (req.user === undefined)
     return res.status(401).json({ message: "Unauthorized" });
 
@@ -46,6 +45,42 @@ router.get("/success-callback", (req, res) => {
   } else {
     res.status(401).json({ message: "User is not logged in" });
   }
+});
+
+router.push("/newlogin", (req, res) => {
+  knex("users")
+    .select("id")
+    .where({ google_id: profile.id })
+    .then((user) => {
+      if (user.length) {
+        console.log(user, "found the users and successfully login");
+        // If user is found, pass the user object to serialize function
+        done(null, user[0]);
+      } else {
+        // If user isn't found, we create a record
+        console.log("cannot find id, insert to knex table");
+        knex("users")
+          .insert({
+            google_id: profile.id,
+            email: profile.email,
+            avatar_url: profile.picture,
+            displayName: profile.displayName,
+            givenName: profile.given_name,
+            familyName: profile.family_name,
+          })
+          .then((user) => {
+            console.log(user, "user");
+            // Pass the user object to serialize function
+            done(null, user[0]);
+          })
+          .catch((err) => {
+            console.log("Error creating a user", err);
+          });
+      }
+    })
+    .catch((err) => {
+      console.log("Error fetching a user", err);
+    });
 });
 
 module.exports = router;
